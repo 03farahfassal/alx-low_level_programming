3d5fee430cdc73350af367d893b31a11dbffb5a3
fassal farah <farahfassal32@gmail.com>
2023-06-15 15:14:23 -0700

    add

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 int main () {
-char str1[100];
-strcpy(str1, "Programming is like building a multilingual puzzle");
-puts(str1);
+char a[100];
+strcpy(a, "\"Programming is like building a multilingual puzzle");
+puts(a);
 return(0);
 }
