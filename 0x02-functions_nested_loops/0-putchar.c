0ed6264a25e7ea21730ac008bd3670dfd9c128f5
fassal farah <farahfassal32@gmail.com>
2023-06-20 10:24:59 -0700

    add

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Fassal Farah
+ *
+ */
+
+#include "main.h"
+#include "putchar.h"
+
+/**
+ * main - Prints "putchar" followed by a new line.
+ *
+ * return: Always 0.
+ */
+
+int main(void)
+{
+	_putchar();
+
+	return (0);
+}	
