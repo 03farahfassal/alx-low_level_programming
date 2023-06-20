7f26c39f1b9af76c4d9faee572544e7db502bf02
fassal farah <farahfassal32@gmail.com>
2023-06-20 13:10:23 -0700

    add

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet
+ * using _putchar function
+ * Return 0
+ * by Fassal Farah
+ */
+void print_alphabet(void)
+
+{
+	int i = 0;
+	char s[] = "abcdefghijklmnopqrstuvwxyz";
+
+	while (s[i])
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}	
