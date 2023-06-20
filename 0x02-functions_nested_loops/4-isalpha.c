7ab3b6add1a2b1778213b6633ef890d7934439ec
fassal farah <farahfassal32@gmail.com>
2023-06-20 14:38:03 -0700

    add

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: is the char to be checked
+ * Return: 1 if char is alpha, otherwise 0.
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
