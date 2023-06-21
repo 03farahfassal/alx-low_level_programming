77f2083e60cd26298a1e6ce81fb942ab02e78902
fassal farah <farahfassal32@gmail.com>
2023-06-21 10:38:39 -0700

    add

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,51 +1,16 @@
 #include "main.h"
+
 /**
-* main - Test function for positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
+
 int main(void)
 {
-int i;
-i = 0;
-positive_or_negative(i);
-return (0);
-}
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-"0-main.c" [New File]                                                                                                        0,0-1         All
+	int i;
 
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
